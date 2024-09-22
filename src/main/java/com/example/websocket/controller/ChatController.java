a5001354e269581a41105acfc83b1564dcf8d9ba
sungho Park <soowkw@naver.com>
2024-09-22 22:50:39 +0900

    09-21 방번호 구분하기 3.0 개념 정리 주석

--- src/main/java/com/example/websocket/controller/ChatController.java
+++ src/main/java/com/example/websocket/controller/ChatController.java
@@ -23,7 +23,7 @@ public class ChatController {
         this.chatRoomRepository = chatRoomRepository;
         this.redisSubscriber = redisSubscriber;
     }
-
+    // 클라에서 방을 생성하면 JPA로 인해 디비에 방을 생성하는 역할을 한다.
     @MessageMapping("/createRoom")
     @SendTo("/topic/roomCreated")
     public ChatRoom createRoom(String roomName) {
@@ -32,24 +32,23 @@ public class ChatController {
         chatRoom = chatRoomRepository.save(chatRoom);
         return chatRoom;
     }
-
+    // 클라에서 api/sendMessage를 부른다면 디비에 입력한 방 번호가 존재하는지 확인 후 레디스쪽에 메시지를 보낸다
     @MessageMapping("/sendMessage")
     public void sendMessage(@Payload ChatRoomDTO chatRoomDTO) throws JsonProcessingException {
         String roomId = chatRoomDTO.getRoomId();
-        String message = chatRoomDTO.getMessage();
         if (chatRoomRepository.existsById(Integer.parseInt(roomId))) {
             redisPublisher.publish(chatRoomDTO);
         } else {
             throw new IllegalStateException("방이없다");
         }
     }
-
+    // 방이 있는지 체크한다.
     @MessageMapping("/checkRoom")
     @SendTo("/topic/roomCheck")
     public boolean checkRoom(String roomId) {
         return chatRoomRepository.existsById(Integer.parseInt(roomId));
     }
-
+    // 에러 코드다.
     @MessageExceptionHandler(IllegalStateException.class)
     @SendTo("/topic/error")
     public String handleIllegalStateException(IllegalStateException e) {
--- src/main/java/com/example/websocket/dao/ChatRoomRepository.java
+++ src/main/java/com/example/websocket/dao/ChatRoomRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
     boolean existsById(int id);
-    ChatRoom save(ChatRoom chatRoom);
 }
