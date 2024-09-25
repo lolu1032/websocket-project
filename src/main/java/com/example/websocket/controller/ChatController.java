f613838d3d8712b34b5968a10fc1beff80bed103
sungho Park <soowkw@naver.com>
2024-09-25 21:59:48 +0900

    09-25 JWT 토큰 1일차

--- src/main/java/com/example/websocket/controller/ChatController.java
+++ src/main/java/com/example/websocket/controller/ChatController.java
@@ -12,6 +12,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
