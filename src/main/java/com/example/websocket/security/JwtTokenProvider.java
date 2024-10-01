03c5de3026db998484f23eb68645f6912086ff7d
sungho Park <soowkw@naver.com>
2024-10-01 18:01:30 +0900

    10-1 jwt정리

--- src/main/java/com/example/websocket/security/JwtTokenProvider.java
+++ src/main/java/com/example/websocket/security/JwtTokenProvider.java
@@ -33,20 +33,24 @@ public class JwtTokenProvider {
     }
 
     public JwtToken generateToken(Authentication authentication) {
-        String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
+        String authorities = authentication.getAuthorities().stream() // Authentication에서 제공해주는 권한 생성
+                .map(GrantedAuthority::getAuthority) // 각 권한을 문자열로 반환한다.
+                .collect(Collectors.joining(",")); // 권한들을 ,로 구분한다 ROLE_USER,ROLE_ADMIN
 
         long now = (new Date()).getTime();
         Date accessTokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
-
+        // AccessToken 생성
         String accessToken = Jwts.builder()
-                .setSubject(authentication.getName())
-                .claim(AUTHORITIES_KEY,authorities)
-                .setExpiration(accessTokenExpiresIn)
-                .signWith(key, SignatureAlgorithm.HS512)
-                .compact();
+                // sub은 JWT에서 미리 정의된 표준 클레임 이름중 하나.
+                // name은 authentication.getName()값
+                .setSubject(authentication.getName()) // payload : "sub" : "name"
+                .claim(AUTHORITIES_KEY,authorities) // payload : "auth" : "ROLE_USER"
+                .setExpiration(accessTokenExpiresIn) // payload : "exp" : 111100 만료시간
+                .signWith(key, SignatureAlgorithm.HS512)//header : "alg" : "HS512"
+                .compact(); // jwt 문자열 생성
 
+        // RefreshToken 생성
+        // 유효 기간이 만료된 토큰을 새로운 토큰으로 발급
         String refreshToken = Jwts.builder()
                 .setExpiration(new Date(now + REFRESH_TOKEN_EXPIRE_TIME))
                 .signWith(key, SignatureAlgorithm.HS512)
@@ -59,17 +63,30 @@ public class JwtTokenProvider {
                 .build();
     }
     public Authentication getAuthentication(String token) {
+        // Payload의 데이터를 복호화하여 Claims 객체로 변환한다.
+        // 예를 들어 Base64URL 형태를 디코딩해서 JSON형태로 다시 만들어 claims 객체에 넣는다
+        // eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9
+        // .eyJzdWIiOiAiam9obmRvZSIsICJhdXRoIjogIlJPTEVfQ1VTVE9NRV8xIiwgImV4cCI6IDE1MTYyMTYyMDIyfQ
+        // .Z6x5LYPb4N-aG8M2-VFchHbCo_OM27R3WXXHxoPnVbM
         Claims claims = parseClaims(token);
-
+        // 복호화한 payload에서 "auth"가 있거나 비어있는지 확인
         if(claims.get("auth") == null) {
             throw new RuntimeException("권한 정보가 없는 토큰입니다.");
         }
-        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get("auth").toString().split(","))
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+
+        Collection<? extends GrantedAuthority> authorities =
+                // 문자열로 변환 후 , 로 나누어 배열을 만든다.
+                Arrays.stream(claims.get("auth").toString().split(","))
+                .map(SimpleGrantedAuthority::new) // SimpleGrantedAuthority 객체로 바꾼다. SimpleGrantedAuthority 은 시큐리티에서 권한을 나타내는 클래스다
+                .collect(Collectors.toList()); // SimpleGrantedAuthority("ROLE_USER") 반환한 객체를 리스트로 수집한다.
+        // UserDetails는 시큐리티에서 사용자정보를 나타내는 객체
+        // sub : 사용자 이름 , password, 권한을 객체 넣느다.
         UserDetails principal = new User(claims.getSubject(),"",authorities);
+        // 시큐리티에서 사용자의 인증 정보를 나타내는 객체를 생성한다.
+        // 사용자정보 , "" , 권한
         return new UsernamePasswordAuthenticationToken(principal,"",authorities);
     }
+    // Signature jwt 검증
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
